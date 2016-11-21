1b85baa56906dd2ac35953c0e34e0550b7ed9d72
Kelsey Ruckert <klr324@psu.edu>
2016-11-21 10:11:29 -0500

    update file location and plotting codes

--- RFILES/Rahmstorf_calibrate/RCP_temp_scenarios.R
+++ RFILES/Rahmstorf_calibrate/RCP_temp_scenarios.R
@@ -5,7 +5,7 @@
 #
 #  -This program loads in temperatures from various RCP scenarios. Using the different temperature
 #       scenarios each method (Bootstrap, MCMC homoskedastic, and MCMC heteroskedastic) is projected.
-#       This script produces supplementary figure 4 in Ruckert et al. (2016). For further description and
+#       This script produces supplementary figure 4 in Ruckert et al. (accepted). For further description and
 #       references, please read the paper.
 #
 #   -NOTE: The graph will be saved as a eps file in the working directory.
@@ -22,10 +22,10 @@
 # install.packages('ncdf')
 library(ncdf)
 library(ncdf4)
-source("Data/temp_sea_2300.R")
+source("../Data/temp_sea_2300.R")
 
 # RCP 2.6 -----------------------------------------------------------------
-# fid1<-open.ncdf("Data/global.tas.aann.HadGEM2-ES.historical+rcp26.r1i1p1.18600101-22991230.nc")
+# fid1<-open.ncdf("../Data/global.tas.aann.HadGEM2-ES.historical+rcp26.r1i1p1.18600101-22991230.nc")
 
 # temp_K<-get.var.ncdf(fid1,"tas")
 # year_nc<-get.var.ncdf(fid1,"time")
@@ -34,7 +34,7 @@ source("Data/temp_sea_2300.R")
 # close.ncdf(fid1)
 
 # If ncdf is unavailable un comment the following code and use ncdf4 library
-fid1 <- nc_open("Data/global.tas.aann.HadGEM2-ES.historical+rcp26.r1i1p1.18600101-22991230.nc")
+fid1 <- nc_open("../Data/global.tas.aann.HadGEM2-ES.historical+rcp26.r1i1p1.18600101-22991230.nc")
 temp_K <- ncvar_get(fid1,"tas")
 year_nc <- ncvar_get(fid1,"time")
 nc_close(fid1)
@@ -49,7 +49,7 @@ tempRCP26_anom = temp_C - respect_20th
 tempRCP26_anom = c(temp.data[1:134,2], tempRCP26_anom[155:241])
 
 # RCP 4.5 -----------------------------------------------------------------
-# fid1<-open.ncdf("Data/global.tas.aann.HadGEM2-ES.historical+rcp45.r1i1p1.18600101-22991230.nc")
+# fid1<-open.ncdf("../Data/global.tas.aann.HadGEM2-ES.historical+rcp45.r1i1p1.18600101-22991230.nc")
 
 # temp_K<-get.var.ncdf(fid1,"tas")
 # year_nc<-get.var.ncdf(fid1,"time")
@@ -58,7 +58,7 @@ tempRCP26_anom = c(temp.data[1:134,2], tempRCP26_anom[155:241])
 # close.ncdf(fid1)
 
 # If ncdf is unavailable un comment the following code and use ncdf4 library
-fid1 <- nc_open("Data/global.tas.aann.HadGEM2-ES.historical+rcp45.r1i1p1.18600101-22991230.nc")
+fid1 <- nc_open("../Data/global.tas.aann.HadGEM2-ES.historical+rcp45.r1i1p1.18600101-22991230.nc")
 temp_K <- ncvar_get(fid1,"tas")
 year_nc <- ncvar_get(fid1,"time")
 nc_close(fid1)
@@ -70,7 +70,7 @@ tempRCP45_anom = temp_C - respect_20th
 tempRCP45_anom = c(temp.data[1:134,2], tempRCP45_anom[155:241])
 
 # RCP 6.0 -----------------------------------------------------------------
-# fid1<-open.ncdf("Data/global.tas.aann.HadGEM2-ES.historical+rcp60.r1i1p1.18600101-20991230.nc")
+# fid1<-open.ncdf("../Data/global.tas.aann.HadGEM2-ES.historical+rcp60.r1i1p1.18600101-20991230.nc")
 
 # temp_K<-get.var.ncdf(fid1,"tas")
 # year_nc<-get.var.ncdf(fid1,"time")
@@ -79,7 +79,7 @@ tempRCP45_anom = c(temp.data[1:134,2], tempRCP45_anom[155:241])
 # close.ncdf(fid1)
 
 # If ncdf is unavailable un comment the following code and use ncdf4 library
-fid1 <- nc_open("Data/global.tas.aann.HadGEM2-ES.historical+rcp60.r1i1p1.18600101-20991230.nc")
+fid1 <- nc_open("../Data/global.tas.aann.HadGEM2-ES.historical+rcp60.r1i1p1.18600101-20991230.nc")
 temp_K <- ncvar_get(fid1,"tas")
 year_nc <- ncvar_get(fid1,"time")
 nc_close(fid1)
@@ -91,7 +91,7 @@ tempRCP60_anom = temp_C - respect_20th
 tempRCP60_anom = c(temp.data[1:134,2], tempRCP60_anom[155:241])
 
 # RCP 8.5 -----------------------------------------------------------------
-# fid1<-open.ncdf("Data/global.tas.aann.HadGEM2-ES.historical+rcp85.r1i1p1.18600101-22991230.nc")
+# fid1<-open.ncdf("../Data/global.tas.aann.HadGEM2-ES.historical+rcp85.r1i1p1.18600101-22991230.nc")
 
 # temp_K<-get.var.ncdf(fid1,"tas")
 # year_nc<-get.var.ncdf(fid1,"time")
@@ -100,7 +100,7 @@ tempRCP60_anom = c(temp.data[1:134,2], tempRCP60_anom[155:241])
 # close.ncdf(fid1)
 
 # If ncdf is unavailable un comment the following code and use ncdf4 library
-fid1 <- nc_open("Data/global.tas.aann.HadGEM2-ES.historical+rcp85.r1i1p1.18600101-22991230.nc")
+fid1 <- nc_open("../Data/global.tas.aann.HadGEM2-ES.historical+rcp85.r1i1p1.18600101-22991230.nc")
 temp_K <- ncvar_get(fid1,"tas")
 year_nc <- ncvar_get(fid1,"time")
 nc_close(fid1)
@@ -126,7 +126,7 @@ boot.rcp26.rate=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc)
 boot.rcp26.proj=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc) # fit.boot_proj is the projected SLR simulation without noise
 
 for(n in 1:NI.boot) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   boot.rcp26.rate[n,] = bootstrap_parameters[n,1]*(tempRCP26_anom - bootstrap_parameters[n,2])
   boot.rcp26.proj[n,1] = bootstrap_parameters[n,3]
   
@@ -156,7 +156,7 @@ boot.rcp45.rate=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc)
 boot.rcp45.proj=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc) # fit.boot_proj is the projected SLR simulation without noise
 
 for(n in 1:NI.boot) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   boot.rcp45.rate[n,] = bootstrap_parameters[n,1]*(tempRCP45_anom - bootstrap_parameters[n,2])
   boot.rcp45.proj[n,1] = bootstrap_parameters[n,3]
   
@@ -186,7 +186,7 @@ boot.rcp60.rate=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc)
 boot.rcp60.proj=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc) # fit.boot_proj is the projected SLR simulation without noise
 
 for(n in 1:NI.boot) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   boot.rcp60.rate[n,] = bootstrap_parameters[n,1]*(tempRCP60_anom - bootstrap_parameters[n,2])
   boot.rcp60.proj[n,1] = bootstrap_parameters[n,3]
   
@@ -216,7 +216,7 @@ boot.rcp85.rate=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc)
 boot.rcp85.proj=mat.or.vec(NI.boot, length(years.rcp)) #(nr,nc) # fit.boot_proj is the projected SLR simulation without noise
 
 for(n in 1:NI.boot) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   boot.rcp85.rate[n,] =  bootstrap_parameters[n,1]*(tempRCP85_anom - bootstrap_parameters[n,2])
   boot.rcp85.proj[n,1] = bootstrap_parameters[n,3]
   
@@ -247,7 +247,7 @@ homo.rcp26.rate=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 homo.rcp26.proj=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:homo_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   homo.rcp26.rate[n,] = homo_sub_chain[n,1]*(tempRCP26_anom - homo_sub_chain[n,2])
   homo.rcp26.proj[n,1] = homo_sub_chain[n,3]
   
@@ -278,7 +278,7 @@ homo.rcp45.rate=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 homo.rcp45.proj=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:homo_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   homo.rcp45.rate[n,] = homo_sub_chain[n,1]*(tempRCP45_anom - homo_sub_chain[n,2])
   homo.rcp45.proj[n,1] = homo_sub_chain[n,3]
   
@@ -309,7 +309,7 @@ homo.rcp60.rate=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 homo.rcp60.proj=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:homo_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (s17)
   homo.rcp60.rate[n,] = homo_sub_chain[n,1]*(tempRCP60_anom - homo_sub_chain[n,2])
   homo.rcp60.proj[n,1] = homo_sub_chain[n,3]
   
@@ -340,7 +340,7 @@ homo.rcp85.rate=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 homo.rcp85.proj=mat.or.vec(homo_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:homo_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   homo.rcp85.rate[n,] = homo_sub_chain[n,1]*(tempRCP85_anom - homo_sub_chain[n,2])
   homo.rcp85.proj[n,1] = homo_sub_chain[n,3]
   
@@ -372,7 +372,7 @@ heter.rcp26.rate=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 heter.rcp26.proj=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:heter_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   heter.rcp26.rate[n,] = heter_sub_chain[n,1]*(tempRCP26_anom - heter_sub_chain[n,2])
   heter.rcp26.proj[n,1] = heter_sub_chain[n,3]
   
@@ -403,7 +403,7 @@ heter.rcp45.rate=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 heter.rcp45.proj=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:heter_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   heter.rcp45.rate[n,] = heter_sub_chain[n,1]*(tempRCP45_anom - heter_sub_chain[n,2])
   heter.rcp45.proj[n,1] = heter_sub_chain[n,3]
   
@@ -434,7 +434,7 @@ heter.rcp60.rate=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 heter.rcp60.proj=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:heter_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   heter.rcp60.rate[n,] = heter_sub_chain[n,1]*(tempRCP60_anom - heter_sub_chain[n,2])
   heter.rcp60.proj[n,1] = heter_sub_chain[n,3]
   
@@ -465,7 +465,7 @@ heter.rcp85.rate=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 heter.rcp85.proj=mat.or.vec(heter_subset_length, length(years.rcp)) #(nr,nc)
 
 for(n in 1:heter_subset_length) {
-  # Estimate the sea level rate of change: equation (1)
+  # Estimate the sea level rate of change: equation (S17)
   heter.rcp85.rate[n,] = heter_sub_chain[n,1]*(tempRCP85_anom - heter_sub_chain[n,2])
   heter.rcp85.proj[n,1] = heter_sub_chain[n,3]
   
@@ -494,7 +494,7 @@ remove(res.heter_85, heter.rcp85.proj, heter.rcp85.rate)
 
 ###############################################################################
 # Plot ------------------------------------------------------------------------
-source("Scripts/put_fig_letter.R")
+source("../Scripts/put_fig_letter.R")
 ######################################## MAIN FIGURES #############################################
 
 # Calculate the 99% confidence interval for each method
@@ -550,7 +550,7 @@ for(i in 1:length(years.rcp)){
 }
 
 #--------------- Estimate survival function
-source("Scripts/plot_sf.r")
+source("../Scripts/plot_sf.r")
 survival.boot.26 <- plot.sf(boot.slr.rcp26[,221]/100, make.plot=F)
 survival.boot.45 <- plot.sf(boot.slr.rcp45[,221]/100, make.plot=F)
 survival.boot.60 <- plot.sf(boot.slr.rcp60[,221]/100, make.plot=F)
@@ -613,15 +613,15 @@ par(mfrow=c(2,2), mgp=c(1.5,.5,0),  mar=c(3.5,4,1,1)) # set figure dimensions
 y_99=c(years.rcp, rev(years.rcp))
 
 # a. RCP8.5
-plot(year, slr/100, type="l", xlab="",ylab="Sea-level anomalies [m]",
+plot(year, slr/100, type="l", xlab="Year",ylab="Sea-level anomalies [m]",
 col=trans_gray,lwd=1,ylim=c(0,1.8), xlim=c(2000,2090))
-#Method C
+# Heteroskedastic
 het_x_99=c(het_rcp85.005, rev(het_rcp85.995))
 polygon(y_99, het_x_99, col=trans_con_colors[3], border=NA)
-#Method B
+# Homoskedastic
 hom_x_99=c(hom_rcp85.005, rev(hom_rcp85.995))
 polygon(y_99, hom_x_99, col=trans_con_colors[2], border=NA)
-#Method A
+# Bootstrap
 boot_x_99=c(boot_rcp85.005, rev(boot_rcp85.995))
 polygon(y_99, boot_x_99, col=trans_con_colors[1], border=NA) # plot 90% polygon
 
@@ -632,19 +632,19 @@ polygon(y_99, boot_x_99, col=trans_con_colors[1], border=NA) # plot 90% polygon
 axis(side=4, labels=FALSE)
 put.fig.letter("a. RCP8.5",font=2,  x=0.15, y=0.98)
 
-legend("topleft", c('99% CI Bootstrap','99% CI Bayesian (homoskedastic)','99% CI Bayesian (heteroskedastic)'), cex=0.85,
+legend("topleft", c('99% CI Bootstrap','99% CI Homoskedastic Bayesian','99% CI Heteroskedastic Bayesian'), cex=0.85,
        pch=15, bty="n", col=trans_con_colors[1:3])
 
 # b. RCP6.0
-plot(year, slr/100, type="l", xlab="",ylab="",
+plot(year, slr/100, type="l", xlab="Year",ylab="Sea-level anomalies [m]",
 col=trans_gray,lwd=1,ylim=c(0,1.8), xlim=c(2000,2090))
-#Method C
+# Heteroskedastic
 het_x_99=c(het_rcp60.005, rev(het_rcp60.995))
 polygon(y_99, het_x_99, col=trans_con_colors[3], border=NA)
-#Method B
+# Homoskedastic
 hom_x_99=c(hom_rcp60.005, rev(hom_rcp60.995))
 polygon(y_99, hom_x_99, col=trans_con_colors[2], border=NA)
-#Method A
+# Bootstrap
 boot_x_99=c(boot_rcp60.005, rev(boot_rcp60.995))
 polygon(y_99, boot_x_99, col=trans_con_colors[1], border=NA) # plot 90% polygon
 
@@ -656,13 +656,13 @@ put.fig.letter("b. RCP6.0",font=2,  x=0.15, y=0.98)
 # c. RCP4.5
 plot(year, slr/100, type="l", xlab="Year",ylab="Sea-level anomalies [m]",
 col=trans_gray,lwd=1,ylim=c(0,1.8), xlim=c(2000,2090))
-#Method C
+# Heteroskedastic
 het_x_99=c(het_rcp45.005, rev(het_rcp45.995))
 polygon(y_99, het_x_99, col=trans_con_colors[3], border=NA)
-#Method B
+# Homoskedastic
 hom_x_99=c(hom_rcp45.005, rev(hom_rcp45.995))
 polygon(y_99, hom_x_99, col=trans_con_colors[2], border=NA)
-#Method A
+# Bootstrap
 boot_x_99=c(boot_rcp45.005, rev(boot_rcp45.995))
 polygon(y_99, boot_x_99, col=trans_con_colors[1], border=NA) # plot 90% polygon
 
@@ -672,15 +672,15 @@ axis(side=4, labels=FALSE)
 put.fig.letter("c. RCP4.5",font=2,  x=0.15, y=0.98)
 
 # d. RCP2.6
-plot(year, slr/100, type="l", xlab="Year",ylab="",
+plot(year, slr/100, type="l", xlab="Year",ylab="Sea-level anomalies [m]",
 col=trans_gray,lwd=1,ylim=c(0,1.8), xlim=c(2000,2090))
-#Method C
+# Heteroskedastic
 het_x_99=c(het_rcp26.005, rev(het_rcp26.995))
 polygon(y_99, het_x_99, col=trans_con_colors[3], border=NA)
-#Method B
+# Homoskedastic
 hom_x_99=c(hom_rcp26.005, rev(hom_rcp26.995))
 polygon(y_99, hom_x_99, col=trans_con_colors[2], border=NA)
-#Method A
+# Bootstrap
 boot_x_99=c(boot_rcp26.005, rev(boot_rcp26.995))
 polygon(y_99, boot_x_99, col=trans_con_colors[1], border=NA) # plot 90% polygon
 
@@ -694,26 +694,25 @@ dev.off()
 
 ################################ survival function ###################################################
 setEPS()
-postscript(file="SuppFigures/SFig7.eps", horizontal = FALSE, onefile = FALSE, paper = "special", family="Helvetica",
+postscript(file="SuppFigures/SFigsurvival_rcpscenarios.eps", horizontal = FALSE, onefile = FALSE, paper = "special", family="Helvetica",
 width=double_column, height=column_height*2, pointsize=11)
 par(mfrow=c(2,2), mgp=c(1.5,.5,0),  mar=c(3.5,4,1,1)) # set figure dimensions
 # survival function for SLR values in 2100
 plot(survival.het.85$sf.num, survival.het.85$sf, log="y", type="l",
-col=trans_con_colors[3], ylab="Survival function
-[1-cumulative probability]", lwd=3,
-xlab="",main="",ylim=c(1e-04,1), yaxt="n")
+col=trans_con_colors[3], ylab="Survival function\n[1-cumulative probability]", lwd=3,
+xlab="Projected sea level 2100 [m]",main="",ylim=c(1e-04,1), yaxt="n")
 lines(survival.boot.85$sf.num, survival.boot.85$sf, lwd=3, col=trans_con_colors[1])
 lines(survival.homo.85$sf.num, survival.homo.85$sf, lwd=3, col=trans_con_colors[2])
 abline(h=1/100, lty=2)
 axis(2, at=10^(-4:0), label=parse(text=paste("10^", -4:0, sep="")))
 text(0.5, 0.015, expression(paste("10"^"-2", sep="")))
-legend("topright", c('Bootstrap','Bayesian (homoskedastic)','Bayesian (heteroskedastic)'), cex=0.85,
+legend("topright", c('Bootstrap','Homoskedastic Bayesian','Heteroskedastic Bayesian'), cex=0.85,
 pch=15, bty="n",col=trans_con_colors[1:3])
 put.fig.letter("a. RCP8.5",font=2,  x=0.15, y=0.98)
 
 plot(survival.het.60$sf.num, survival.het.60$sf, log="y", type="l",
-col=trans_con_colors[3], ylab="", lwd=3,
-xlab="",main="",ylim=c(1e-04,1), yaxt="n")
+col=trans_con_colors[3], ylab="Survival function\n[1-cumulative probability]", lwd=3,
+xlab="Projected sea level 2100 [m]",main="",ylim=c(1e-04,1), yaxt="n")
 lines(survival.boot.60$sf.num, survival.boot.60$sf, lwd=3, col=trans_con_colors[1])
 lines(survival.homo.60$sf.num, survival.homo.60$sf, lwd=3, col=trans_con_colors[2])
 abline(h=1/100, lty=2)
@@ -721,9 +720,8 @@ axis(2, at=10^(-4:0), label=parse(text=paste("10^", -4:0, sep="")))
 put.fig.letter("b. RCP6.0",font=2,  x=0.15, y=0.98)
 
 plot(survival.het.45$sf.num, survival.het.45$sf, log="y", type="l",
-col=trans_con_colors[3], ylab="Survival function
-[1-cumulative probability]", lwd=3,
-xlab="Projected sea-level 2100 [m]",main="",ylim=c(1e-04,1), yaxt="n")
+col=trans_con_colors[3], ylab="Survival function\n[1-cumulative probability]", lwd=3,
+xlab="Projected sea level 2100 [m]",main="",ylim=c(1e-04,1), yaxt="n")
 lines(survival.boot.45$sf.num, survival.boot.45$sf, lwd=3, col=trans_con_colors[1])
 lines(survival.homo.45$sf.num, survival.homo.45$sf, lwd=3, col=trans_con_colors[2])
 abline(h=1/100, lty=2)
@@ -731,8 +729,8 @@ axis(2, at=10^(-4:0), label=parse(text=paste("10^", -4:0, sep="")))
 put.fig.letter("c. RCP4.5",font=2,  x=0.15, y=0.98)
 
 plot(survival.het.26$sf.num, survival.het.26$sf, log="y", type="l",
-col=trans_con_colors[3], ylab="", lwd=3,
-xlab="Projected sea-level 2100 [m]",main="",ylim=c(1e-04,1), yaxt="n")
+col=trans_con_colors[3], ylab="Survival function\n[1-cumulative probability]", lwd=3,
+xlab="Projected sea level 2100 [m]",main="",ylim=c(1e-04,1), yaxt="n")
 lines(survival.boot.26$sf.num, survival.boot.26$sf, lwd=3, col=trans_con_colors[1])
 lines(survival.homo.26$sf.num, survival.homo.26$sf, lwd=3, col=trans_con_colors[2])
 abline(h=1/100, lty=2)
