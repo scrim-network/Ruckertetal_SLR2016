6d81c30189056569762a0eb507699a5913d0743a
Kelsey Ruckert <klr324@psu.edu>
2016-11-21 14:42:54 -0500

    update file location

--- RFILES/Toy_calibrate/ToyScripts/min_res.R
+++ RFILES/Toy_calibrate/ToyScripts/min_res.R
@@ -4,10 +4,10 @@
 #  - Author: Kelsey Ruckert (klr324@psu.edu)
 #
 #  -This function find the sum of the absolute residuals estimated from the model
-#       function in "DEoptim_rahm_model.R". Finding the sum is used with the DEoptim
+#       function in "de_boot.R". Finding the sum is used with the DEoptim
 #       R function to minimize the residuals and find the best values for the
 #       parameters. The best parameter values are used as initial values for the
-#       bootstrap and MCMC codes as described in Ruckert et al. (GRL 2016).
+#       bootstrap and MCMC codes as described in Ruckert et al. (2016).
 #
 # THIS CODE IS PROVIDED AS-IS WITH NO WARRANTY (NEITHER EXPLICIT
 # NOT IMPLICIT).  I SHARE THIS CODE IN HOPES THAT IT IS USEFUL, 
@@ -22,7 +22,7 @@
 
 min_res = function(p){
   sum(abs(obs - model(p))) # returns the sum of the absolute values
-  # from sea-level minus simulated model values
+  # from observations minus simulated model values
 }
 
 ################################## END ###########################################
